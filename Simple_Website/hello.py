2302e79c5c2489285a3c77d8fc80d24069557306
Fu-Yilei <fuyilei96@outlook.com>
2017-05-03 18:26:04 -0700

    add show group stage & search

--- Simple_Website/hello.py
+++ Simple_Website/hello.py
@@ -5,27 +5,21 @@ Created on Sun Apr 23 10:59:55 2017
 @author: Frank Fu
 """
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import flask
 import sqlite3 as lite
 import sys
 import urllib
 from bs4 import BeautifulSoup
-from flask.ext.sqlalchemy import SQLALchemy
-from flask.ext.script import Manager
-from sqlalchemy import text
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:fuyilei@96@localhost:3306/fifa2014'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
-manager = Manager(app)
+
 
 def read_news():
     html = urllib.request.urlopen("http://www.espnfc.com/index").read()
     soup = BeautifulSoup(html, "lxml")
-    all_news = (soup.findChildren('h2'))
+    all_news = (soup.findChildren('h2'))[0:5]
     f=open('./static/news.json','w')
     for i in range(5):
         f.write(str(all_news[i]))
@@ -35,19 +29,28 @@ def read_news():
 
 @app.route('/')
 def index(name=None):
-    read_news()
-    return render_template('index.html',  name = name)
+    return render_template('index.html', **locals())
 # @app.route('/questions/<int:question_id>'):    #int has been used as a filter that only integer will be passed in the url otherwise it will give a 404 error
-@app.route('/teams/')
 @app.route('/teams/<name>')
 def team(name=None):
-    result = db.engine.execute("select * from clubs")
-    return render_template('teams.html',  name=name)
+    teamname = name
+    con = lite.connect("fifa2014.db")
+    cur = con.cursor()
+    cur.execute("select * from group_stage where Group_Name = \'"+str(name[-1])+"\'")
+    rows = cur.fetchall()
+    return render_template('teams.html', **locals())
 
-@app.route('/tables')
-def table(name=None):
-    return render_template('tables.html', name=name)
-
+@app.route('/search', methods = ["GET","POST"])
+def search(name=None):
+    if request.method == "GET":
+        return render_template('search.html', name=name)
+    else:
+        search = request.form["search"]
+        con = lite.connect("fifa2014.db")
+        cur = con.cursor()
+        cur.execute("select * from players where Name like \'%"+str(search)+"%\'")
+        rows = cur.fetchall()
+        return render_template('search.html',  **locals())
 @app.route('/matches')
 def match(name=None):
     return render_template('matches.html', name=name)

84da9727044a45d34bd821360a13833914bdce31
Fu-Yilei <fuyilei96@outlook.com>
2017-05-05 11:57:19 -0700

    finish!

--- Simple_Website/hello.py
+++ Simple_Website/hello.py
@@ -53,21 +53,76 @@ def search(name=None):
         cur.execute("select * from players inner join clubs on players.club_id = clubs.club_id where Name like \'%"+str(search)+"%\'")
         rows = cur.fetchall()                                                                                                                                                                                                                                                                                                                                                           
         return render_template('search.html',  **locals())
-@app.route('/matches')
-def match(name=None):
-    return render_template('matches.html', name=name)
-
-@app.route('/stats')
-def stat(name=None):
-    stat_type = name
+    
+@app.route('/matches', methods = ["GET","POST"])
+def matches(name=None):
+    Cmnt =  []
     con = lite.connect("fifa2014.db")
     cur = con.cursor()
-    cur.execute("select * from \'"+str(name)+"\'")
+    cur.execute("select * from matches")
+    rows = cur.fetchall()
     
-    if stat_type == "referees":
-        cur.execute("select * from \'"+str(name)+"\'")
-    elif stat_type == "players":
-        pass
+    curc = con.cursor()
+    curc.execute("select * from comment")
+    c = curc.fetchall()
+    if request.method == "GET":
+        return render_template('matches.html', **locals())
+    else:
+        comment = request.form["comment"]
+        author = request.form["author"]
+        matchid = request.form["matchid"]
+        with con:
+            cur = con.cursor()
+            cur.execute("insert into comment (match_id, comment, author) values ('{}', '{}','{}')".format(matchid, comment, author))  
+        return render_template('matches.html', **locals())
+@app.route('/comments')
+def comments(name=None):
+    con = lite.connect("fifa2014.db")
+    cur = con.cursor()
+    cur.execute("select * from comment inner join matches on comment.match_id = matches.match_id")
+    rows = cur.fetchall()
+    return render_template('comments.html', **locals())
+@app.route('/matchdata/<name>')
+def matchdata(name = None):
+    con = lite.connect("fifa2014.db")
+    cur = con.cursor()
+    cur.execute("select * from lineups inner join players on lineups.Player_id = Players.id where lineups.match_id = "+name)
+    rows = cur.fetchall()
+    cur.execute("select * from goals inner join players on goals.Player_id = Players.id where goals.match_id = "+name)
+    goals = cur.fetchall()
+    cur.execute("select * from matches where match_id = "+name)
+    matches = cur.fetchall()
+    link = '/matchdata/'+str(name)
+    return render_template('matchdata.html', **locals())
+
+@app.route('/stats/<name>')
+def stat(name=None):
+    con = lite.connect("fifa2014.db")
+    cur = con.cursor()
+
+    if name == "referees":
+        cur.execute("select * from referees")
+    elif name == "players":
+        cur.execute("select p.Name, p.Pos, P.Jersey_No, c.Country_Name, cl.Club_Name, \
+                                    g.goal_count, g.penalty_goals, g.own_goals, \
+                                    d.total_yc, d.total_rc \
+                    from players p, countries c, clubs cl \
+                    inner join ( \
+                                select Player_ID, count(*) as goal_count, \
+                                sum(case when Special_Type = 'PENALTY' then 1 else 0 end) as penalty_goals, \
+                                sum(case when Special_Type = 'OWN_GOAL' then 1 else 0 end) as own_goals \
+                                from goals \
+                                group by Player_ID \
+                                ) as g \
+                    on p.Id = g.Player_ID \
+                    inner join ( \
+                                select Player_ID, \
+                                sum(case when Card = 'YC' then 1 else 0 end) as total_yc, \
+                                sum(case when Card = 'RC' then 1 else 0 end) as total_rc \
+                                from discipline \
+                                group by Player_ID) as d \
+                    on p.Id = d.Player_ID \
+                    where p.Country_ID = c.Country_ID and p.Club_ID = cl.Club_ID")
 
     rows = cur.fetchall()
     return render_template('stats.html', **locals())
